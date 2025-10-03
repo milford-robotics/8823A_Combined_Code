e1812f028cf3b737a4b6d80e180651ebb5fb3df6
MWed-7 <144293278+MWed-7@users.noreply.github.com>
2025-10-02 20:29:57 -0400

    Skills Auton getting better and better. As of now, it can score 4 in the low and makes it to the second long goal.

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -77,18 +77,6 @@ void setAllMotorPorts(){
 vex::brain       Brain;
 controller Controller1 = controller(primary);
 
-/*
-motor LeftFront = motor(PORT11, ratio6_1, true);
-motor LeftMiddle = motor(PORT12, ratio6_1, true);
-motor LeftRear = motor(PORT13, ratio6_1, true);
-motor RightFront = motor(PORT18, ratio6_1, false);
-motor RightMiddle = motor(PORT19, ratio6_1, false);
-motor RightRear = motor(PORT20, ratio6_1, false);
-motor UpperIntake = motor(PORT10, ratio6_1, false);
-motor MiddleIntake = motor(PORT1, ratio6_1, true);
-motor LowerIntake = motor(PORT14, ratio6_1, false);
-*/
-
 
 motor LeftFront = motor(PORT11, ratio6_1, true);
 motor LeftMiddle = motor(PORT12, ratio6_1, true);
