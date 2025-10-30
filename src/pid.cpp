ef8eaf39dc500a992049dc46c3438d0c1e591a4d
Tyler Harpster <tylerharpster@mevsd.net>
2025-10-30 19:28:52 -0400

    hhhhj

--- src/pid.cpp
+++ src/pid.cpp
@@ -1,47 +1,46 @@
 #include <vex.h>
 
-class pid
+class PID_Controller
 {
 private:
-    double ki, kp, kd, dt;
+    float tol; //constants
+    float ki, kp, kd; //the big three
+    float integral, error, derivative, olderror, dt, start; //things changed in pid loop
+    float anti_wind; //other things that can be changed
 public:
-    pid();
-    void setParams(){
-        
-    };
+    void setParams(float kp,float ki,float kd);
+    void setParams(float kp,float ki);
+    float calculate (int angle){ // Turn function
+        this->error=angle-(InertialSensor.rotation()-this->start); //get error
+        if(fabs(this->error)+fabs(this->olderror)>tol){ //if pid isnt right
+
+            float speed=ki*integral+kp*error; //calculate target speed
+            if(fabs(this->error)<=fabs(angle*this->anti_wind)) integral+=error*0.005; //update integral
+            this->olderror=this->error; //set old error
+            return speed<-80?-80:speed>80?80:speed; //return (-80<error<80)
+        }
+        else{
+            StopDriveTrain();
+            printf("done \n\n\n");
+            return 0;
+        }
+
+    }
+    void reset(){
+        this->olderror=0.0/0.0;
+    }
+
 };
 
-pid::pid(){
+void PID_Controller::setParams(float kp,float ki,float kd){
+    this->kp=kp;
+    this->ki=ki;
+    this->kd=kd;
 
 }
-
-
-
-void pid (int angle){ // Turn function
-  auto clamp=[](float val,float bottom,float top){return std::min(std::max(val,bottom),top);};
-  float error=0, tol=0.2,start=InertialSensor.rotation(), speed=0, ki=0.2,kp=0.25,integral=0,oldError;
-
-  do{
-    oldError=error;
-    
-    vex::task::sleep(10);
-
-    printf("e: %.2f h: %.2f i: %.2f s: %.2f \n",error,InertialSensor.rotation(),integral, speed);
-    error=angle-(InertialSensor.rotation()-start);
-    speed=ki*integral+kp*error;
-    speed=clamp(speed,-80,80);
-    if(fabs(error)<=fabs(angle*0.3)) integral+=error*0.005;
-
-    LeftFront.spin(forward,speed,percent);
-    LeftMiddle.spin(forward,speed,percent);
-    LeftRear.spin(forward,speed,percent);
-    RightFront.spin(reverse,speed,percent);
-    RightMiddle.spin(reverse,speed,percent);
-    RightRear.spin(reverse,speed,percent);
-
-  }while(fabs(error)+fabs(oldError)>tol);
-  StopDriveTrain();
-  printf("done \n\n\n");
-  vex::task::sleep(1000);
+void PID_Controller::setParams(float kp,float ki){
+    this->kp=kp;
+    this->ki=ki;
+    this->kd=0;
 
 }
